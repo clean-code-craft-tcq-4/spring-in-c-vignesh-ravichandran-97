cfe962e8c22bde6fcab315b8fe49e4add7685f71
vignesh-ravichandran-97 <101125124+vignesh-ravichandran-97@users.noreply.github.com>
2022-05-18 23:00:30 +0530

    check_alert

--- stats.c
+++ stats.c
@@ -25,3 +25,11 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+   if(computedStats.max>maxThreshold)
+   {
+          alerters[0]();
+          alerters[1]();
+   }
+}
